0aed50d6f4a12b527d44a5a16354d2feabc0557a
Stephanos Malikotsinas <31777077+circular-code@users.noreply.github.com>
2018-03-26 11:20:50 +0200

    Create patterns.js

---
+++ patterns.js
@@ -0,0 +1,32 @@
+// --- Module Pattern (aka Object Literal) ---
+
+var testModule = {
+ 
+    variableKey: variableValue,
+ 
+    functionKey: function () {
+      // ...
+    }
+};
+
+
+// --- Revealing Module Pattern (aka Module Pattern) ---
+
+var testModule = (function () {
+ 
+  var counter = 0;
+ 
+  return {
+ 
+    incrementCounter: function () {
+      return counter++;
+    },
+ 
+    resetCounter: function () {
+      console.log( "counter value prior to reset: " + counter );
+      counter = 0;
+    }
+  };
+ 
+})();
+ 
