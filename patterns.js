346d807519dc5b4a545a9d12ed940eb93402f614
Stephanos Malikotsinas <31777077+circular-code@users.noreply.github.com>
2018-05-05 15:27:49 +0200

    es5/6 classes and inheritance

--- patterns.js
+++ patterns.js
@@ -65,3 +65,94 @@ var Singleton = (function () {
         }
     };
 })();
+
+// --- ES5 Class ---
+
+// Constructor
+function MyClass() {
+  this.a = 42;
+}
+
+// Method
+MyClass.prototype.method = function method() {};
+
+// Instantiate
+var instance = new MyClass();
+
+// --- ES5 Inheritance ---
+
+// Parent class constructor
+function Parent() {
+  this.a = 42;
+}
+
+// Parent class method
+Parent.prototype.method = function method() {};
+
+// Child class constructor
+function Child() {
+  Parent.call(this);
+  this.b = 3.14159
+}
+
+// Inherit from the parent class
+Child.prototype = Object.create(Parent.prototype);
+Child.prototype.constructor = Child;
+
+// Child class method
+Child.prototype.method = function method() {
+  Parent.prototype.method.call(this);
+};
+
+// Instantiate
+var instance = new Child();
+
+// --- ES6 Class --
+
+// Class
+class MyClass {
+
+  // Constructor
+  constructor() {
+    this.a = 42;
+  }
+
+  // Method
+  method() {}
+}
+
+// Instantiate
+let instance = new MyClass();
+
+// --- ES6 Inheritance ---
+
+// Parent class
+class Parent {
+
+  // Parent class constructor
+  constructor() {
+    this.a = 42;
+  }
+
+  // Parent class method
+  method() {}
+
+}
+
+// Child class inherits from Parent
+class Child extends Parent {
+
+  // Child class constructor
+  constructor() {
+    super();
+    this.b = 3.14159;
+  }
+
+  // Child class method
+  method() {
+    super.method();
+  }
+}
+
+// Instantiate
+let instance = new Child();
